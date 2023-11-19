7a2efd379c050102d66733d271bf2575a9f2d4c1
repsandcode <albertjonathan.dev@gmail.com>
2023-11-19 17:40:58 +0800

    add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
+import CreateUser from "./components/CreateUser";
+import EditUser from "./components/EditUser";
+import ListUser from "./components/ListUser";
+
 function App() {
-  return <div className="App">Hello React</div>;
+  return (
+    <div className="App">
+      <h5>React CRUD operations using PHP API and MySQL</h5>
+
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">List Users</Link>
+            </li>
+            <li>
+              <Link to="/users/create">Create User</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route index element={<ListUser />} />
+          <Route path="/users/create" element={<CreateUser />} />
+          <Route path="/users/:id/edit" element={<EditUser />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
